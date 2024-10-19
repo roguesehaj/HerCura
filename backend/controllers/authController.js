ddd0c62df1bfc7d3486819557dcb3266c54e53de
Sehaj <arorasehajbir@gmail.com>
2024-10-19 10:52:41 +0530

    init

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,3 +2,10 @@ const User = require('../models/Users');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+exports.register = async (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    res.status(201).send('User registered');
+};

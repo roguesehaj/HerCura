1c63d15eb336dfe030c7220fc109f4c1c7beb66e
Sehaj <arorasehajbir@gmail.com>
2024-10-19 10:57:24 +0530

    init

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -9,3 +9,14 @@ exports.register = async (req, res) => {
     await user.save();
     res.status(201).send('User registered');
 };
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (user && await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY);
+        res.json({ token });
+    } else {
+        res.status(401).send('Invalid credentials');
+    }
+};
